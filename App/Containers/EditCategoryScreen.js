e79bafe1f434860eca979f1a7920b996292ec77d
Tyler Bailey <tyler@elexicon.com>
2017-10-20 23:55:11 -0400

    Add Edit Categories functionality

--- App/Containers/EditCategoryScreen.js
+++ App/Containers/EditCategoryScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Container, Content } from 'native-base'
 import { connect } from 'react-redux'
+import Realm from 'realm'
+import { CategorySchema } from '../Fixtures/BudgetSchemas'
+import CategoryModel from '../Fixtures/CategoryModel'
 import CategoryForm from '../Components/CategoryForm'
 
 // Styles
@@ -12,8 +15,34 @@ class EditCategoryScreen extends Component {
     super(props)
   }
 
-  _handleCategoryEdit (category) {
+  _handleCategoryEdit(cid, title) {
+    if(cid !== '' && typeof id !== undefined) {
+      try {
+        Realm.open({
+          path: 'CategorySelect.realm',
+          schema: [CategorySchema]
+        }).then(realm => {
+          try {
+            realm.write(() => {
+              realm.create('Category', new CategoryModel(cid, title), true)
+            })
 
+            const {navigate} = this.props.navigation
+
+            navigate(
+              'CategoriesScreen', {
+                updated: true
+              }
+            )
+          } catch (e) {
+            console.log('Error saving category: ' + e)
+          }
+        })
+
+      } catch (e) {
+        console.log('Error opening Category table: ' + e)
+      }
+    }
   }
 
   render () {
@@ -21,8 +50,12 @@ class EditCategoryScreen extends Component {
 
     return (
       <Container style={styles.container}>
-        <Content scrollEnabled={false}>
-          <CategoryForm cid={data.cid} catTitle={data.catTitle} editing={data.editing} navigation={this.props.navigation} handler={() => this._handleCategoryEdit(category)} />
+        <Content scrollEnabled={false} keyboardShouldPersistTaps="never">
+          <CategoryForm
+            catTitle={data.catTitle}
+            cid={data.cid}
+            handler={(cid, title) => this._handleCategoryEdit(cid, title)}
+          />
         </Content>
       </Container>
     )
