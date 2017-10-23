6b63556ac08dcdecd5279c181bef56ec21bc30e2
Tyler Bailey <tyler@elexicon.com>
2017-10-23 18:25:45 -0400

    Connect Add Category modal form to realm. Style the add category modal

--- App/Components/AddCategoryModal.js
+++ App/Components/AddCategoryModal.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import { Button, Icon, Text, View } from 'native-base'
-import { Modal } from 'react-native'
+import Realm from 'realm'
+import { CategorySchema } from '../Fixtures/BudgetSchemas'
+import CategoryModel from '../Fixtures/CategoryModel'
+import { Button, Icon, Text } from 'native-base'
+import { View, Modal, TouchableOpacity } from 'react-native'
 import CategoryForm from './CategoryForm'
 import styles from './Styles/AddCategoryModalStyle'
 
@@ -26,18 +29,44 @@ export default class AddCategoryModal extends Component {
 		this.props.modalHandler(visible)
 	}
 
+	_handleNewCategory(id, title) {
+		if(id === '' || id === null) {
+			try {
+				Realm.open({
+					path: 'CategorySelect.realm',
+					schema: [CategorySchema]
+				}).then(realm => {
+					try {
+						realm.write(() => {
+							realm.create('Category', new CategoryModel(null, title))
+						})
+
+						this._handleCloseModal(false)
+					} catch (e) {
+						console.log('Error category: ' + e)
+					}
+				})
+
+			} catch (e) {
+				console.log('Error opening Category table: ' + e)
+			}
+		}
+	}
+
   render () {
     return (
-      <View style={{marginTop: 22}}>
+      <View style={{marginTop: 34}}>
         <Modal
 					animationType="slide"
-					transparent={false}
+					transparent={true}
 					visible={this.state.modalVisible}
 				>
-					{
-						//<Icon name="ios-add-outline" style={{color: '#FFF', fontSize: 12}} onPress={() => this._handleCloseModal(false)} />
-					}
-					<CategoryForm cid='' catTitle='' editing={false}/>
+					<View style={{marginTop: 64, flex: 1}}>
+						<TouchableOpacity onPress={() => this._handleCloseModal(false)}>
+							<Icon name="ios-close-outline" style={styles.closeButton} />
+						</TouchableOpacity>
+						<CategoryForm cid='' catTitle='' editing={false} handler={(id, title) => this._handleNewCategory(id, title)}/>
+					</View>
         </Modal>
       </View>
     )
--- App/Components/Styles/AddCategoryModalStyle.js
+++ App/Components/Styles/AddCategoryModalStyle.js
@@ -3,5 +3,12 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
   container: {
     flex: 1
-  }
+  },
+	closeButton: {
+		fontSize: 36,
+		color: '#FFF',
+		textAlign: 'right',
+		marginRight: 15,
+		marginTop: 15
+	}
 })
