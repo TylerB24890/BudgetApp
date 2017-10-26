b51743a69fe45fa34a11093a1b32141636ff79ef
Tyler Bailey <tyler@elexicon.com>
2017-10-26 10:08:16 -0400

    Remove currency format function from BudgetBalance for Android compatibility (replaced with toFixed() for now)

--- App/Components/BudgetBalance.js
+++ App/Components/BudgetBalance.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 // import PropTypes from 'prop-types';
 import { View, Text, TouchableOpacity } from 'react-native'
 import BalanceMessage from './BalanceMessage'
-import { CurrencyFormat } from '../Utils/CurrencyFormat'
+//import { CurrencyFormat } from '../Utils/CurrencyFormat'
 import styles, { positiveColor, negativeColor } from './Styles/BudgetBalanceStyle'
 
 export default class BudgetBalance extends Component {
@@ -41,11 +41,11 @@ export default class BudgetBalance extends Component {
       <View style={styles.screenHeader}>
         <View style={styles.starting}>
 					<TouchableOpacity onPress={() => this.props.navigation.navigate('SettingsScreen', { starting: this.state.starting })}>
-						<Text style={styles.screenTitle}>Starting Balance: <Text style={styles.starting}>{CurrencyFormat.format(this.state.starting)}</Text></Text>
+						<Text style={styles.screenTitle}>Starting Balance: <Text style={styles.starting}>${this.state.starting.toFixed(2)}</Text></Text>
 					</TouchableOpacity>
         </View>
         <View style={styles.available}>
-          <Text style={styles.screenTitle}>Available: <Text style={{color: this.state.balanceColor}}>{CurrencyFormat.format(this.state.balance)}</Text></Text>
+          <Text style={styles.screenTitle}>Available: <Text style={{color: this.state.balanceColor}}>${this.state.balance.toFixed(2)}</Text></Text>
         </View>
 
 				<BalanceMessage user={this.state.user} balance={this.state.balance} starting={this.state.starting} navigation={this.props.navigation} />
