6e6624e639bef73682749c4827545e1f510e9004
Tyler Bailey <tyler@elexicon.com>
2017-10-26 13:11:42 -0400

    Remove unused function from CurrencyFormat utility

--- App/Utils/CurrencyFormat.js
+++ App/Utils/CurrencyFormat.js
@@ -1,10 +1,3 @@
-/*export const CurrencyFormat = (num) => {
-  var p = num.toFixed(2).split(".");
-  return "$" + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
-      return  num=="-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
-  }, "") + "." + p[1];
-}*/
-
 export const CurrencyFormat = (num) => {
   return num.toLocaleString('en-US', { style: 'currency', currency: 'USD'})
 }
