8078e672515ae1311fc6d69053da069bcc1a6314
Tyler Bailey <tyler@elexicon.com>
2017-10-31 18:41:45 -0400

    Build out intro screens

--- App/Containers/Styles/SumthingIntroScreenStyle.js
+++ App/Containers/Styles/SumthingIntroScreenStyle.js
@@ -1,24 +1,51 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { ApplicationStyles, Metrics } from '../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
 	slide: {
 		flex: 1,
 		paddingHorizontal: 15,
-		paddingVertical: 30
+		paddingVertical: 30,
+		position: 'relative'
 	},
 	slideHeader: {
 		textAlign: 'center',
 		color: '#FFF',
-		marginBottom: 10
+		marginBottom: 10,
+		alignSelf: 'center'
+	},
+	slideImageContainer: {
+		marginBottom: 15,
+	},
+	slideImage: {
+		width: 181,
+		height: 350,
+		alignSelf: 'center'
 	},
 	slideText: {
 		color: '#FFF',
-		textAlign: 'center'
+		textAlign: 'center',
+		alignSelf: 'center'
 	},
 	slideTextBold: {
 		color: '#FFF',
 		fontWeight: '700'
-	}
+	},
+	skipContainer: {
+		marginTop: 12,
+		justifyContent: 'center'
+	},
+	skip: {
+		color: '#FFF',
+		fontSize: 18,
+		fontWeight: '700',
+		alignSelf: 'center'
+	},
+	swipe: {
+		color: '#7f8c8d',
+		fontSize: 12,
+		alignSelf: 'center',
+		marginLeft: 5
+	},
 })
--- App/Containers/SumthingIntroScreen.js
+++ App/Containers/SumthingIntroScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { Image } from 'react-native'
+import { Image, TouchableOpacity } from 'react-native'
 import { Container, View, Text, H1, Footer } from 'native-base'
 import AppIntro from 'react-native-app-intro'
 
@@ -14,14 +14,18 @@ class SumthingIntroScreen extends Component {
 		super(props)
 	}
 
-	_onSkipBtnHandle = (index) => {
-		const data = this.props.navigation.state.params
-    this.props.navigation.navigate('BudgetView', {new: data.new})
+	_onSkipBtnHandle = () => {
+		this._onDoneBtnHandle()
   }
 
   _onDoneBtnHandle = () => {
-		const data = this.props.navigation.state.params
-    this.props.navigation.navigate('BudgetView', {new: data.new})
+		var newUser = false
+
+		if(typeof this.props.navigation.state.params !== 'undefined' && typeof this.props.navigation.state.params.new !== 'undefined') {
+			newUser = this.props.navigation.state.params.new
+		}
+
+    this.props.navigation.navigate('BudgetView', {new: newUser})
   }
 
   _onnextBtnHandle = (index) => {
@@ -38,41 +42,73 @@ class SumthingIntroScreen extends Component {
 			<AppIntro>
 
         <View style={[styles.slide,{ backgroundColor: '#1abc9c' }]}>
+					<View level={15} style={styles.slideImageContainer}>
+						<Image style={styles.slideImage} source={require('../Images/intro-screen1.png')} />
+					</View>
           <View level={10}>
 						<H1 style={styles.slideHeader}>Manage your expenses</H1>
 					</View>
           <View level={5}>
 						<Text style={styles.slideText}><Text style={styles.slideTextBold}>on the go</Text> by swiping left from the home screen. Edit or delete an expense by tapping it.</Text>
 					</View>
+					<View style={styles.skipContainer}>
+						<TouchableOpacity onPress={() => this._onSkipBtnHandle()}>
+							<Text style={styles.skip}>Skip <Text style={styles.swipe}>(1/4)</Text></Text>
+						</TouchableOpacity>
+					</View>
         </View>
 
 
 				<View style={[styles.slide,{ backgroundColor: '#2ecc71' }]}>
+					<View level={15} style={styles.slideImageContainer}>
+						<Image style={styles.slideImage} source={require('../Images/intro-screen2.png')} />
+					</View>
 					<View level={10}>
 						<H1 style={styles.slideHeader}>Create categories</H1>
 					</View>
           <View level={5}>
 						<Text style={styles.slideText}>to see where your money is going and how much. Categories give you the ability to delete a group of expenses in one click. Perfect for one time expenses!</Text>
 					</View>
+					<View style={{marginTop: 8}}>
+						<TouchableOpacity onPress={() => this._onSkipBtnHandle()}>
+							<Text style={styles.skip}>Skip <Text style={styles.swipe}>(2/4)</Text></Text>
+						</TouchableOpacity>
+					</View>
         </View>
 
 
 				<View style={[styles.slide,{ backgroundColor: '#3498db' }]}>
+					<View level={15} style={styles.slideImageContainer}>
+						<Image style={styles.slideImage} source={require('../Images/intro-screen3.png')} />
+					</View>
 					<View level={10}>
 						<H1 style={styles.slideHeader}>Set a starting balance</H1>
 					</View>
           <View level={5}>
 						<Text style={styles.slideText}>to keep track of how much money you have left. Try setting your starting balance to your paycheck and budget from there.</Text>
 					</View>
+					<View style={styles.skipContainer}>
+						<TouchableOpacity onPress={() => this._onSkipBtnHandle()}>
+							<Text style={styles.skip}>Skip <Text style={styles.swipe}>(3/4)</Text></Text>
+						</TouchableOpacity>
+					</View>
         </View>
 
 
 				<View style={[styles.slide,{ backgroundColor: '#9b59b6' }]}>
+					<View level={15} style={styles.slideImageContainer}>
+						<Image style={styles.slideImage} source={require('../Images/intro-screen4.png')} />
+					</View>
 					<View level={10}>
 						<H1 style={styles.slideHeader}>Track your expenses</H1>
 					</View>
           <View level={5}>
 						<Text style={styles.slideText}><Text style={styles.slideTextBold}>your way.</Text> Sumthing gives you the freedom and flexibility to categorize and manage your expenses how you see fit. <Text style={styles.slideTextBold}>Get creative!</Text></Text>
+					</View>
+					<View style={styles.skipContainer}>
+						<TouchableOpacity onPress={() => this._onDoneBtnHandle()} style={{paddingHorizontal: 10, paddingVertical: 5}}>
+							<Text style={styles.skip}>Done</Text>
+						</TouchableOpacity>
 					</View>
         </View>
       </AppIntro>
